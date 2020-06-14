3dd14fc09007a960ec62c50f6e411b397fbd7f7b
DrInnovatio <turing1723@gmail.com>
2020-06-14 20:53:13 +1000

    added the enter the name screen

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,14 +1,31 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
 
 const TextScreen = () => {
+  const [name, setName] = useState('');
   return (
     <View>
-      <Text>Text Screen</Text>
+      <Text>Enter the name : </Text>
+      <TextInput
+        style={ styles.input }
+        autoCapitalize="none"
+        autoCorrect={ false }
+        value={ name }
+        onChangeText={ (newValue) => setName(newValue) }
+      />
+      <Text>My name is { name }</Text>
+      { false ? <Text>That was true.</Text> : null }
     </View>
   )
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    margin: 15,
+    borderColor: 'black',
+    borderWidth: 1,
+    fontSize: 15
+  }
+});
 
 export default TextScreen;
