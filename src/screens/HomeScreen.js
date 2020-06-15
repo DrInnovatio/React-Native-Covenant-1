ffff6dc7204f28b29cd3ae5edf39c301fe963ca6
DrInnovatio <turing1723@gmail.com>
2020-06-15 16:09:54 +1000

    added Box Screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -34,7 +34,10 @@ const HomeScreen = ({ navigation }) => {
         title="Go to Text Demo"
         onPress={ () => navigation.navigate('Text') }
       />
-
+      <Button
+        title="Go to Box Screen"
+        onPress={ () => navigation.navigate('Box') }
+      />
 
       {/* <TouchableOpacity onPress={() => props.navigation.navigate('List')}>
       <Text>Go to List Demo</Text>
