6431cb0073bea18f1280444dbe4fa2e33dccd4cd
DrInnovatio <turing1723@gmail.com>
2020-06-16 12:06:11 +1000

    Flex box question solution

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -4,21 +4,40 @@ import { Text, View, StyleSheet } from 'react-native';
 const BoxScreen = () => {
   return (
     <View style={ styles.viewStyle }>
-      <Text style={ styles.textStyle }>Box Screen</Text>
+      <Text style={ styles.textOneStyle }>child #1</Text>
+      <Text style={ styles.textTwoStyle }>child #2</Text>
+      <Text style={ styles.textThreeStyle }>child #3</Text>
     </View>
   )
 };
 
 const styles = StyleSheet.create({
   viewStyle: {
-    borderWidth: 1,
-    borderColor: 'blue'
+    borderWidth: 3,
+    borderColor: 'black',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    height: 350
+
   },
-  textStyle: {
-    borderWidth: 1,
+  textOneStyle: {
+    height: 100,
+    width: 100,
     borderColor: 'red',
-    marginVertical: 20,
-    marginHorizontal: 20
+    backgroundColor: 'red'
+  },
+  textTwoStyle: {
+    height: 100,
+    width: 100,
+    borderColor: 'green',
+    backgroundColor: 'green',
+    top: 100
+  },
+  textThreeStyle: {
+    height: 100,
+    width: 100,
+    borderColor: 'blue',
+    backgroundColor: 'blue'
   }
 });
 
